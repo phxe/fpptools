e377a126fd71695485be68704834ec846cdaee3d
Mohammad Alam <najmi.alam@yahoo.com>
2022-03-10 21:44:03 -0800

    intellisense test

---
+++ src/test/testintelliSENSE.ts
@@ -0,0 +1,31 @@
+{
+    // test on intellisense rules
+    
+    // Controls quick suggestions
+    "editor.quickSuggestions": {
+        "other": true,
+        "comments": false,
+        "strings": false;
+    },
+
+     // Provides confirmation if sugesstions are accepted
+    "editor.acceptSuggestionOnCommitCharacter": true,
+
+    // Controls suggestions
+    "editor.acceptSuggestionOnEnter": "on",
+
+    // suggestion speed
+    "editor.quickSuggestionsDelay": 5,
+
+    // Suggest certain characters controlling suggetions
+    "editor.suggestOnTriggerCharacters": true,
+
+    // it will show preselctions from list suggestion 
+    "editor.suggestSelection": "recentlyUsed",
+
+    // Enable word based suggestions
+    "editor.wordBasedSuggestions": true,
+
+    // Enable the parameter hints
+    "editor.parameterHints.enabled": true,
+}
