6d8a22502b067f5de8a9a2adf3935572719964c7
Daniel Martindale <47557499+phxe@users.noreply.github.com>
2022-05-05 09:42:08 -0700

    Add CompletionItemKinds

--- src/completionProvider.ts
+++ src/completionProvider.ts
@@ -2,8 +2,27 @@ import * as vscode from "vscode";
 import * as FPP from "./constants";
 import { SemanticTokens } from "./semanticTokens";
 
+// export const KeywordCompletionMap = {
+
+//   ARRAY: TokenType.VARIABLE,
+//   COMPONENT: TokenType.COMPONENT,
+//   CONSTANT: TokenType.VARIABLE,
+//   INSTANCE: TokenType.INSTANCE,
+//   ENUM: TokenType.ENUM,
+//   ENUMMEMBER: TokenType.ENUMMEMBER,
+//   MODULE: TokenType.NAMESPACE,
+//   PORT: TokenType.PORT,
+//   STRUCT: TokenType.STRUCT,
+//   STRUCTMEMBER: TokenType.VARIABLE,
+//   TYPE: TokenType.TYPE,
+//   TOPOLOGY: TokenType.TOPOLOGY,
+//   PARAM: TokenType.PARAMETER,
+//   SPECIFIER: TokenType.SPECIFIER,
+// };
+
 export class CompletionItems implements vscode.CompletionItemProvider {
   static keywordList: vscode.CompletionItem[] = [];
+  static keywordCompletionMap = new Map<FPP.TokenType, vscode.CompletionItemKind>();
 
   constructor() {
     let k: keyof typeof FPP.Keywords;
@@ -13,6 +32,18 @@ export class CompletionItems implements vscode.CompletionItemProvider {
       // Add additional text edits for some keywords
       CompletionItems.keywordList.push(completionItem);
     }
+    CompletionItems.keywordCompletionMap.set(FPP.TokenType.VARIABLE, vscode.CompletionItemKind.Variable);
+    CompletionItems.keywordCompletionMap.set(FPP.TokenType.COMPONENT, vscode.CompletionItemKind.Class);
+    CompletionItems.keywordCompletionMap.set(FPP.TokenType.INSTANCE, vscode.CompletionItemKind.Class);
+    CompletionItems.keywordCompletionMap.set(FPP.TokenType.ENUM, vscode.CompletionItemKind.Enum);
+    CompletionItems.keywordCompletionMap.set(FPP.TokenType.ENUMMEMBER, vscode.CompletionItemKind.EnumMember);
+    CompletionItems.keywordCompletionMap.set(FPP.TokenType.NAMESPACE, vscode.CompletionItemKind.Module);
+    CompletionItems.keywordCompletionMap.set(FPP.TokenType.PORT, vscode.CompletionItemKind.Method);
+    CompletionItems.keywordCompletionMap.set(FPP.TokenType.STRUCT, vscode.CompletionItemKind.Struct);
+    CompletionItems.keywordCompletionMap.set(FPP.TokenType.TYPE, vscode.CompletionItemKind.TypeParameter);
+    CompletionItems.keywordCompletionMap.set(FPP.TokenType.TOPOLOGY, vscode.CompletionItemKind.Class);
+    CompletionItems.keywordCompletionMap.set(FPP.TokenType.PARAMETER, vscode.CompletionItemKind.Variable);
+    CompletionItems.keywordCompletionMap.set(FPP.TokenType.SPECIFIER, vscode.CompletionItemKind.Function);
   }
 
   provideCompletionItems(
@@ -30,7 +61,7 @@ export class CompletionItems implements vscode.CompletionItemProvider {
 
         SemanticTokens.identifiers.forEach((t, str) => {
           if (t[0] !== "" && c.substring(c.length - t[0].length, c.length) === t[0]) {
-            let completionItem = new vscode.CompletionItem(str, vscode.CompletionItemKind.Variable);
+            let completionItem = new vscode.CompletionItem(str, CompletionItems.keywordCompletionMap.get(t[1]));
             completionItem.detail = t[1];
             list.push(completionItem);
           }
@@ -38,7 +69,7 @@ export class CompletionItems implements vscode.CompletionItemProvider {
         return list;
       }
       SemanticTokens.identifiers.forEach((t, str) => {
-        let completionItem = new vscode.CompletionItem(str, vscode.CompletionItemKind.Variable);
+        let completionItem = new vscode.CompletionItem(str, CompletionItems.keywordCompletionMap.get(t[1]));
         completionItem.detail = t[1];
         list.push(completionItem);
       });
